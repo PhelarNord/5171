4680926cf77c9537a46469251974d57271a0e7db
Patrik Nord <patrik_nord@hotmail.com>
2019-11-22 21:26:32 +0100

    First test

--- SPI bus/spi.js
+++ SPI bus/spi.js
@@ -1,4 +1,24 @@
 const spi = require('spi-device');
-var pre = spi.open(0,0 )
-
-false
+ 
+// The pre is on bus 0 and it's device 0
+const pre = spi.open(0, 0, err => {
+  // An SPI message is an array of one or more read+write transfers
+  const message = [{
+    sendBuffer: Buffer.from([0,0,]), // Sent to read channel 5
+    receiveBuffer: Buffer.alloc(1),              // Raw data read from channel 5
+    byteLength: 1,
+    speedHz: 10000000 // Use a low bus speed to get a good reading from the TMP36
+  }];
+ 
+  if (err) throw err;
+ 
+  pre.transfer(message, (err, message) => {
+    if (err) throw err;
+ 
+    // Convert raw value from sensor to celcius and log to console
+    const data = message[0].receiveBuffer[1];      
+   
+ 
+    console.log(celcius);
+  });
+});
