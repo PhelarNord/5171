2fe085fb6d1a5225ee91b1225a3bf28598bd1eb6
Patrik Nord <patrik_nord@hotmail.com>
2019-11-23 16:25:47 +0100

    Uppdated buffer length

--- SPI bus/spi.js
+++ SPI bus/spi.js
@@ -4,9 +4,9 @@ const spi = require('spi-device');
 const pre = spi.open(0, 0, err => { // Is this right for my device?
   // An SPI message is an array of one or more read+write transfers
   const message = [{
-    sendBuffer: Buffer.from([0,0,]), //Does binary work here or hex?
-    receiveBuffer: Buffer.alloc(1),              
-    byteLength: 1,
+    sendBuffer: Buffer.from([0,0]), //Does binary work here or hex?
+    receiveBuffer: Buffer.alloc(2),              
+    byteLength: 2,
     speedHz: 10000000 // Does this work? 10Mhz or do i get like 7,8
   }];
  
@@ -15,7 +15,7 @@ const pre = spi.open(0, 0, err => { // Is this right for my device?
   pre.transfer(message, (err, message) => {
     if (err) throw err;
  
-    // Convert raw value from sensor to celcius and log to console
+    // Convert raw value from data and log to console
     const data = message[0].receiveBuffer[1];      
    
  
